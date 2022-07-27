1a77caf2ff9bb34d44319c7e7f31c86a85fbd398
Diego Bryan <thediegobryan@gmail.com>
2022-07-27 19:55:18 -0400

    changed following in user model to not be required

--- ecards/models.py
+++ ecards/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    following = models.ManyToManyField("User", related_name='followers')
+    following = models.ManyToManyField("User", related_name='followers', null=True, blank=True)
     def __str__(self):
         return self.username
 
