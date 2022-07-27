322f64a0c485e7f55344d16f1f3bac1cbfb6117d
Diego Bryan <thediegobryan@gmail.com>
2022-07-27 13:33:32 -0400

    finished seeting up models and migrated

--- ecards/models.py
+++ ecards/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
+    following = models.ManyToManyField("User", related_name='followers')
     def __str__(self):
         return self.username
 
@@ -29,23 +30,38 @@ class Card(models.Model):
         (DANCING_SCRIPT, 'Dancing Script'),
     ]
 
+    WHITE = f"#FFFFFF"
+    RED = f"#FF0000"
+    BLACK = f"#000000"
+    BLUE = f"#0086FF"
+    GREEN = f"#04D61F"
+    YELLOW = f"#FFFD00"
+    ORANGE = f"#FF7F00"
+    PURPLE = f"#D100FF"
+    BROWN = f"#845A0F"
+
+    COLOR_CHOICES = [
+        (WHITE, 'White'),
+        (RED, 'Red'),
+        (BLACK, 'Black'),
+        (BLUE, 'Blue'),
+        (GREEN, 'Green'),
+        (YELLOW, 'Yellow'),
+        (ORANGE, 'Orange'),
+        (PURPLE, 'Purple'),
+        (BROWN, 'Brown'),
+    ]
+
     user_id = models.ForeignKey("User", on_delete=models.CASCADE, related_name='cards')
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=255, null=True, blank=True)
     message = models.CharField(max_length=255)
     font = models.TextField(choices=FONT_CHOICES, default=ROBOTO)
     font_color = models.TextField(null=True, blank=True)
-    bg_color = models.TextField()
-    border_color = models.TextField()
+    bg_color = models.TextField(choices=COLOR_CHOICES, default=WHITE)
+    border_color = models.TextField(choices=COLOR_CHOICES, default=BLACK)
     border_style = models.TextField(null=True, blank=True)
     img_src = models.TextField(null=True, blank=True)
     
     def __str__(self):
         return f"{self.user_id} created a post at {self.created_at}"
-
-class Follow(models.Model):
-    user_id = models.ForeignKey("User", on_delete=models.CASCADE, related_name='follows')
-    follow_user_id = models.ForeignKey("User", on_delete=models.CASCADE, related_name='follows')
-
-    def __str__(self):
-        return f"{self.user_id} followed {self.follow_user_id}"
