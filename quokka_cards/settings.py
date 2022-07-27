f9542ba9c0e23a10ba4c3a2ee7928fd7b9dbfddb
Metta Rolando <mnrolando3@gmail.com>
2022-07-27 11:06:58 -0400

    Updated staticfiles to static

--- quokka_cards/settings.py
+++ quokka_cards/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 # STATIC_ROOT = (BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'staticfiles']
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
