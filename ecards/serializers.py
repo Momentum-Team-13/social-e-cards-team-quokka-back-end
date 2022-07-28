bd17e8f875525c02f3d10d1744dd826b40c0c6c3
Metta Rolando <mnrolando3@gmail.com>
2022-07-28 13:07:21 -0400

    Uncommented out line in card serializer to finalize merge with user_list branch

--- ecards/serializers.py
+++ ecards/serializers.py
@@ -16,23 +16,22 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class CardListSerializer(serializers.ModelSerializer):
-    # # Uncomment when User Serializer is merged with this
-    # user_id = UserSerializer(read_only=True)
+    user_id = UserSerializer(read_only=True)
 
     class Meta:
         model = Card
         fields = (
             'id',
-            'user_id', 
-            'created_at', 
-            'title', 
-            'message', 
-            'font', 
-            'font_color', 
-            'bg_color', 
-            'border_color', 
-            'border_style', 
-            'img_src'
+            'user_id',
+            'created_at',
+            'title',
+            'message',
+            'font',
+            'font_color',
+            'bg_color',
+            'border_color',
+            'border_style',
+            'img_src',
             )
 
 
@@ -41,14 +40,14 @@ class NewCardSerializer(serializers.ModelSerializer):
         model = Card
         fields = (
             'id',
-            'user_id', 
-            'created_at', 
-            'title', 
-            'message', 
-            'font', 
-            'font_color', 
-            'bg_color', 
-            'border_color', 
-            'border_style', 
-            'img_src'
+            'user_id',
+            'created_at',
+            'title',
+            'message',
+            'font',
+            'font_color',
+            'bg_color',
+            'border_color',
+            'border_style',
+            'img_src',
             )
