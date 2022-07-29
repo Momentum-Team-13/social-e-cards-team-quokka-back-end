486aa1eeab57c4f13b7c5903abc87a33af64495f
Diego Bryan <thediegobryan@gmail.com>
2022-07-28 21:12:25 -0400

    added default url for page ''

--- quokka_cards/urls.py
+++ quokka_cards/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('api/auth/', include('djoser.urls')),
     path("api/auth/", include("djoser.urls.authtoken")),
     path('users/', ecard_views.UserList.as_view(), name='user-list'),
+    path('', ecard_views.cardlist.as_view(), name='card-list'),
     path('cards/', ecard_views.cardlist.as_view(), name='card-list'),
     path('cards/new/', ecard_views.new_card.as_view(), name='new-card'),
     path('cards/<int:pk>/', ecard_views.carddetail.as_view(), name='card-detail'),
