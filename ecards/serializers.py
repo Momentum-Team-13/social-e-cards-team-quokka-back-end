11a8c1f07ad2be2c0055bcc5c5ea3e2a9475fe46
Diego Bryan <thediegobryan@gmail.com>
2022-07-28 21:06:17 -0400

    added permissions to card endpoints

--- ecards/serializers.py
+++ ecards/serializers.py
@@ -1,10 +1,11 @@
+from asyncio.format_helpers import _format_callback_source
 from rest_framework import serializers
 from .models import User, Card
 
 
 class UserSerializer(serializers.ModelSerializer):
     cards = serializers.PrimaryKeyRelatedField(many=True, queryset=Card.objects.all())
-
+    
     class Meta:
         model = User
         fields = [
